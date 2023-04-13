ff35300fbad83f8dfac1bed9351b1dc11c6a2b1e
younggraham-t <younggraham.t@gmail.com>
2023-04-13 09:06:41 -0400

    Initial Commit

---
+++ FloatyBall/FloatyBallApp.swift
@@ -0,0 +1,17 @@
+//
+//  FloatyBallApp.swift
+//  FloatyBall
+//
+//  Created by Graham on 4/13/23.
+//
+
+import SwiftUI
+
+@main
+struct FloatyBallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
